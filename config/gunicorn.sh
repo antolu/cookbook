b00a4f233071df3a880a3af21fcd8dad12ac198f
Anton Lu <anton96lu@gmail.com>
2020-04-18 16:42:36 +0000

    completet link gunicorn <-> nginx w/ autodeploy

---
+++ config/gunicorn.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NAME="cookbook"                                  
+DJANGODIR=/home/cookbook
+SOCKFILE=/var/run/cookbook.sock
+USER=devuser
+GROUP=webapps
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=webapps.settings
+DJANGO_WSGI_MODULE=webapps.wsgi
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ./venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ./venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
--- tests/tools.sh
+++ tests/tools.sh
@@ -3,6 +3,12 @@
 CWD=$PWD
 SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
+if [[ -z $INSTALLDIR ]]; then
+    INSTALLDIR=$PWD
+fi
+
+DOCKER=TRUE # need to replace this line with dynamic detection
+
 init() {
     git submodule sync --recursive
     git submodule update --init --recursive --progress
@@ -17,15 +23,33 @@ devinit() {
     DOCKER=TRUE
 }
 
+installPackages() {
+    yum install -y epel-release
+    yum install -y supervisor
+
+    systemctl enable --now supervisord
+}
+
 setupPython() {
     if [[ -d "./venv" ]]; then
         echo "venv already installed. Skipping. "
         return 0
     fi
 
+    echo "Creating virtualenv"
+
     python3 -m virtualenv ./venv
     . ./venv/bin/activate
     pip install -r requirements.txt
+
+    chown -R devuser ./venv
+}
+
+configureSupervisor() {
+    cp -f ./config/supervisor.cookbook.ini /etc/supervisord.d/cookbook.ini
+
+    supervisorctl reread
+    supervisorctl update
 }
 
 setupNginx() {
@@ -36,12 +60,14 @@ setupNginx() {
 
         # need to restore SELinux permissions for created folder in the future
     fi
+
+    echo "Inserting nginx blocks"
     sed -i '/\s{4}server {/,/\s{4}}/ s/^/#/' /etc/nginx/nginx.conf
 
     # Need to make sure not to insert line twice (upon reinstall or similar)
     sed -i '/include \/etc\/nginx\/conf.d\/\*.conf;/a \\n    include \/etc\/nginx\/sites-enabled\/\*.conf;' /etc/nginx/nginx.conf
 
-    cp -f ./tests/nginx/cookbook.conf /etc/nginx/sites-available/cookbook.conf
+    cp -f ./config/nginx.cookbook.conf /etc/nginx/sites-available/cookbook.conf
     ln -s /etc/nginx/sites-available/cookbook.conf /etc/nginx/sites-enabled/cookbook.conf
 
     systemctl restart nginx
@@ -50,18 +76,36 @@ setupNginx() {
 }
 
 setupEnv() {
-    SETUPUSER="
-    setupPython
-    "
-    SETUPADMIN="
-    setupNginx
-    "
     if [[ -z $DOCKER ]]; then
-        docker exec -itw /home/cookbook server bash -c "source tests/tools.sh && $SETUPADMIN"
-        docker exec -it -u devuser -w /home/cookbook server bash -c "source tests/tools.sh && $SETUPUSER"
+        echo "In docker environment. Setting up inside Docker."
+        docker exec -itw /home/cookbook -e INSTALLDIR:$INSTALLDIR server bash -c "source tests/tools.sh && setup"
     else
-        setupPython
+        echo "In production environment. Setting up normally."
+        setup
     fi
+}
+
+createUserGroups() {
+    groupadd --system webapps
+    useradd  --system --gid webapps --home $INSTALLDIR cookbook
+
+    if [[ -z $DOCKER ]]; then
+        chown -R cookbook:users $INSTALLDIR
+    else
+        usermod -aG webapps devuser
+    fi
+}
+
+setup() {
+    echo "INSTALLDIR is $INSTALLDIR"
+
+    installPackages
+
+    setupPython
+    setupNginx
+
+    configureSupervisor
+    createUserGroups
 }
 
 # TODO: install texlive
