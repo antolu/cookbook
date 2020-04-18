568acb1ba54faa58e43deb9d3bb754c22074e0e5
Anton Lu <anton96lu@gmail.com>
2020-04-18 17:06:55 +0000

    Django skeleton, gunicorn <-> django

--- django/gunicorn.sh
+++ django/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME="cookbook"                                  
+NAME="cookbook"
 DJANGODIR=/home/cookbook
 SOCKFILE=/var/run/cookbook.sock
 USER=devuser
@@ -13,7 +13,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source ./venv/bin/activate
+source ../venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
@@ -23,7 +23,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ./venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec ../venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
