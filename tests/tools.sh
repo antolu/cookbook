77d7099fc63941952d7250878ed65a2adbee93f4
Anton Lu <anton96lu@gmail.com>
2020-06-27 02:01:49 +0200

    cd back to cwd

--- tests/tools.sh
+++ tests/tools.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 installStylesheet () {
+  CWD=$PWD
   cd /tmp
 
   git clone https://gitlab.haochen.lu/queubit/cookbook-stylesheet.git cookbook-stylesheet
   cd cookbook-stylesheet
   bash install.sh
+  cd $CWD
 }
 
 runAll() {
